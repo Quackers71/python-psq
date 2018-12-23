c5788eff216ed4d48d634fab97c6b9a8197f8df2
quackers71 <24840026+Quackers71@users.noreply.github.com>
2018-12-23 14:29:47 +0000

    amended py3.7 dependancy and loops.py

---
+++ pspstart/loops.py
@@ -0,0 +1,7 @@
+student_names = ["James", "Katarina", "Jesscica", "Mark", "Bort", "Frank Grimes","Max Power"]
+
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found Him!")
+    print("Currently testing " + name)
