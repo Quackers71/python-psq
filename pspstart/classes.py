86c8920dfd65121d00d650384c11eda2ad72ecb0
quackers71 <24840026+Quackers71@users.noreply.github.com>
2018-12-29 09:00:12 +0000

    Constructor and other special methods

--- pspstart/classes.py
+++ pspstart/classes.py
@@ -2,12 +2,14 @@ students = []
 
 
 class Student:
-    def add_student(self, name, student_id=332):
+    def __init__(self, name, student_id=332):
         student = {"name": name, "student_id": student_id}
         students.append(student)
 
+    def __str__(self):
+        return "Student"
 
-student = Student()
-student.add_student("Mark")
+mark = Student("Mark")
 
-print(students)
+print(mark)
+
