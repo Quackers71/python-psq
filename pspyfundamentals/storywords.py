30bd2e366812a30730234a23ceda0489c5a039bb
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-12-31 16:13:00 +0000

    included shorter method using a join

--- pspyfundamentals/storywords.py
+++ pspyfundamentals/storywords.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+
 with urlopen('http://sixty-north.com/c/t.txt') as story:
     story_words = []
     for line in story:
@@ -13,3 +14,7 @@ for i in story_words:
     full_str += str(i) + " "
 full_str = full_str[:-1]
 print(full_str)
+
+# or try this
+story_words = ' '.join(map(str, story_words))
+print(story_words)
