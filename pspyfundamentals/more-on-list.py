c8d5760574a1000f7212cf05b985337fbe13b514
Quacks <24840026+Quackers71@users.noreply.github.com>
2019-01-10 21:31:22 +0000

    all about, more on list

---
+++ pspyfundamentals/more-on-list.py
@@ -0,0 +1,46 @@
+def ret():
+    print("\r")
+
+
+ret()
+w = "the quick brown fox jumps over the lazy dog".split()
+print("w =", w)
+ret()
+
+i = w.index('fox')
+print("i = w.index('fox'):", i)
+ret()
+
+# i = w.index('unicorn')
+# print("i = w.index('unicorn'):", i)
+# ret()
+# Produces a value error
+
+print("37 in [1, 78, 9, 37, 34, 53] =", 37 in [1, 78, 9, 37, 34, 53])
+print("78 not in [1, 78, 9, 37, 34, 53] =", 78 not in [1, 78, 9, 37, 34, 53])
+ret()
+
+u = "jackdaws love my big sphinx of quartz".split()
+print(u)
+u.remove('jackdaws')
+print("u.remove('jackdaws')")
+print(u)
+ret()
+
+del u[u.index('quartz')]
+print("del u[u.index('quartz')]")
+print(u)
+ret()
+
+# u.remove('pyramid')
+# Produces a value error
+
+a = "I accidentally the whole universe".split()
+print(a)
+
+a.insert(2, "destroyed")
+print(a)
+
+' '.join(a)
+print(a)
+
