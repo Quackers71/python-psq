fd1ce8246e44d87f90a04a85c0d7b0ce904aa8b0
Quackers <24840026+Quackers71@users.noreply.github.com>
2019-02-07 16:28:29 +0000

    Bitwise Operators & Fractal Images

--- pspyfundamentals/bmp.py
+++ pspyfundamentals/bmp.py
@@ -5,7 +5,7 @@ def write_grayscale(filename, pixels):
     """Creates and writes a grayscale BMP file.
 
     Args:
-        filename: The name of the BMP file to be created.
+        filename: The name of the BMP file to me created.
 
         pixels: A rectangular image stored as a sequence of rows.
             Each row must be an iterable series of integers in the
@@ -21,13 +21,13 @@ def write_grayscale(filename, pixels):
         # BMP Header
         bmp.write(b'BM')
 
-        size_bookmark = bmp.tell()      # The next four bytes hold the filesize as 32-bit
-        bmp.write(b'\x00\x00\x00\x00')   # little-endian integer. Zero placeholder for now.
+        size_bookmark = bmp.tell()      # The next four bytes hold the filesize as a 32-bit
+        bmp.write(b'\x00\x00\x00\x00')  # little-endian integer. Zero placeholder for now.
 
         bmp.write(b'\x00\x00')  # Unused 16-bit integer - should be zero
         bmp.write(b'\x00\x00')  # Unused 16-bit integer - should be zero
 
-        pixel.offset_bookmark = bmp.tell()  # The next four bytes hold the integer offset
+        pixel_offset_bookmark = bmp.tell()  # The next four bytes hold the integer offset
         bmp.write(b'\x00\x00\x00\x00')      # to the pixel data. Zero placeholder for now.
 
         # Image Header
@@ -49,10 +49,10 @@ def write_grayscale(filename, pixels):
 
         # Pixel data
         pixel_data_bookmark = bmp.tell()
-        for row in reversed(pixels):    # BMP files are bottom to top
-            row.data = bytes(row)
+        for row in reversed(pixels):  # BMP files are bottom to top
+            row_data = bytes(row)
             bmp.write(row_data)
-            padding = b'\x00' * (4 - (len(row) % 4))    # Pad row to multiple of four bytes
+            padding = b'\x00' * ((4 - (len(row) % 4)) % 4)  # Pad row to multiple of four bytes
             bmp.write(padding)
 
         # End of file
@@ -65,4 +65,44 @@ def write_grayscale(filename, pixels):
         # Fill in pixel offset placeholder
         bmp.seek(pixel_offset_bookmark)
         bmp.write(_int32_to_bytes(pixel_data_bookmark))
-        
+
+
+def _int32_to_bytes(i):
+    """Convert an integer to four bytes in little-endian format."""
+    return bytes((i & 0xff,
+                  i >> 8 & 0xff,
+                  i >> 16 & 0xff,
+                  i >> 24 & 0xff))
+
+
+def _bytes_to_int32(b):
+    """Convert a bytes object containing four bytes into an integer."""
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
+
+
+def dimensions(filename):
+    """Determine the dimensions in pixels of a BMP image.
+
+    Args:
+        filename: The filename of a BMP file.
+
+    Returns:
+        A tuple containing two integers with the width
+        and height in pixels.
+
+    Raises:
+        ValueError: If the file was not a BMP file.
+        OSError: If there was a problem reading the file.
+    """
+
+    with open(filename, 'rb') as f:
+        magic = f.read(2)
+        if magic != b'BM':
+            raise ValueError("{} is not a BMP file".format(filename))
+
+        f.seek(18)
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
+
+        return (_bytes_to_int32(width_bytes),
+                _bytes_to_int32(height_bytes))
