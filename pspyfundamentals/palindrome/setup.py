d8ec97d841e163af773964810c6ab1b365b253d7
Quackers <24840026+Quackers71@users.noreply.github.com>
2019-02-08 17:10:12 +0000

    Distributing your Programs with python setup.py install

---
+++ pspyfundamentals/palindrome/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrome'],
+
+    # metadata
+    author='Quackers',
+    author_email='q@quacks.com',
+    description='A module for finding palidrome numbers.',
+    licence='Public domain',
+    keywords='example',
+)
