04dba9e373412d1888a17b073fcb940ab09b25e4
quackers71 <24840026+Quackers71@users.noreply.github.com>
2019-02-09 15:59:06 +0000

    Packages and Modules differences

---
+++ beyondthebasics/modules.py
@@ -0,0 +1,12 @@
+import urllib
+import urllib.request
+
+print(type(urllib))
+print(type(urllib.request))
+print(urllib.__path__)
+
+print(urllib.request.__path__)
+
+# Packages are generally directories
+
+# Modules are generally files
