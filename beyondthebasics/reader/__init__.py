4afb39ebe7248cb38203d0f41d1ef45d09a69d86
Quackers <24840026+Quackers71@users.noreply.github.com>
2019-02-11 16:30:40 +0000

    added __init__.py & reader.py

---
+++ beyondthebasics/reader/__init__.py
@@ -0,0 +1,11 @@
+print('reader is being imported')
+
+# C:\Users\***\Desktop\python-psq\beyondthebasics>python
+# Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:57:15) [MSC v.1915 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import reader
+# >>> type(reader)
+# <class 'module'>
+# >>> reader.__file__
+# 'C:\\Users\\***\\Desktop\\python-psq\\beyondthebasics\\reader\\__init__.py'
+# >>>
