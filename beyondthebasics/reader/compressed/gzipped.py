9797e4baaecb11ab4f7f0dd9cb95eb7eb90be630
quackers71 <24840026+Quackers71@users.noreply.github.com>
2019-02-16 15:09:45 +0000

    Subpackages

---
+++ beyondthebasics/reader/compressed/gzipped.py
@@ -0,0 +1,17 @@
+import gzip
+import sys
+
+opener = gzip.open
+
+if __name__ == '__main__':
+    f = gzip.open(sys.argv[1], mode='wt')
+    f.write(' '.join(sys.argv[2:]))
+    f.close()
+
+# from the cmd line
+# >>> import reader
+# reader is being imported
+# >>> import reader.compressed
+# >>> import reader.compressed.gzipped
+# >>> import reader.compressed.bzipped
+# >>> 
